057420d430952c2ffd79f29f337f8a54b1c08e4e
William Thompson <will.thompson@outlook.com>
2020-11-06 14:41:18 -0800

    Bug fix for ds9show on MacOS

--- src/Display.jl
+++ src/Display.jl
@@ -28,9 +28,9 @@ function ds9show(imgs...; lock=true)
     if Sys.iswindows()
         cmd = `C:\\SAOImageDS9\\ds9.exe $fnames`        
     elseif Sys.isapple() && isdir("/Applications/SAOImageDS9.app")
-        cmd = `open --wait-apps /Applications/SAOImageDS9.app $fnames`
+        cmd = `open -W /Applications/SAOImageDS9.app $fnames`
     elseif Sys.isapple() && isdir("/Applications/SAOImage DS9.app")
-        cmd = `open --wait-apps /Applications/SAOImage\ DS9.app $fnames`
+        cmd = `open -W /Applications/SAOImage\ DS9.app $fnames`
     else
         @warn "Untested system for ds9show. Assuming ds9 is in PATH." maxlog=1
         cmd = `ds9 $fnames`
