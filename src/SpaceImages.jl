802cc56335f71d6cb0f4bbb722654e0cca78cb5d
William Thompson <will.thompson@outlook.com>
2020-11-06 14:10:47 -0800

    Add help message

--- src/SpaceImages.jl
+++ src/SpaceImages.jl
@@ -1,3 +1,27 @@
+"""
+SpaceImages.jl
+
+A package of useful functions for interacting with FITS files.
+
+# Available Functions
+
+Spimage(array)
+headers(img)
+axes(img)
+origin(img)
+size(img)
+copy(img)
+arraydata(img) Gives underlying array
+
+(All other Julia array functions as well)
+
+readfits
+writefits
+readheader  Only read headers. readfits gives both data and headers.
+
+contrast
+
+"""
 module SpaceImages
 
 include("Type.jl")
