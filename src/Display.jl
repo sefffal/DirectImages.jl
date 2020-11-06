72874ebaf2c3a808f91e463f5ba981cf83af347e
William Thompson <will.thompson@outlook.com>
2020-11-06 13:27:18 -0800

    Add ds9show function

--- src/Display.jl
+++ src/Display.jl
@@ -1,18 +1,74 @@
 
 
 
-# imview(image::AbstractArray) = Gray.(image)
-
 using Plots
 using RecipesBase
 using Measures
 using Statistics
-# using ColorSchemes
 
+
+
+using FITSIO
+
+"""
+    ds9show(images..., [lock=true])
+
+Open one or more images in a SAO DS9 window.
+By default, the scales and zooms of the images are locked.
+Pass lock=false to disable.
+"""
+function ds9show(imgs...; lock=true)
+    # http://ds9.si.edu/doc/ref/command.html#fits
+
+    fnames = String[]
+    for img in imgs
+        tempfile = tempname()
+        writefits(tempfile, img)
+        push!(fnames, tempfile)
+    end
+
+    if Sys.iswindows()
+        cmd = `C:\\SAOImageDS9\\ds9.exe $fnames`
+    elseif Sys.isapple()
+        cmd = `/Applications/SAOImage\ DS9.app/Contents/MacOS/ds9 $fnames`
+    else
+        @warn "Untested system for ds9show. Assuming ds9 is in PATH."
+        cmd = `ds9 $fnames`
+    end
+
+    if lock
+        cmd = `$cmd -lock frame image -lock crosshair image -lock crop image -lock slice image -lock bin yes -lock axes yes -lock scale yes -lock scalelimits yes -lock colorbar yes -lock block yes -lock smooth yes -lock 3d yes`
+    end
+
+    task = @async begin
+        try
+            # Open DS9
+            run(cmd)
+        finally
+            # Then delete files whenever it's done
+            @info "cleaning up tmp. files from DS9" fnames
+            rm.(fnames)
+        end
+        return nothing
+    end
+
+    return task
+end
+export ds9show
 
 
 export imshow
 
+
+"""
+    imshow(img; τ=5, lims=1200)
+
+^ displays an image with +5 std, -2.5std, xlims and ylims set to 1200 mas.
+
+Display an image with convenient formatting.
+Looks into the FITS headers for the platescale, if available.
+
+"""
 function imshow(img; τ=nothing, lims=nothing   )
 
 
